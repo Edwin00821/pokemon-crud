f4e98482ef3ca1a9293f1ee907cba7e9c7683d15
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2022-10-20 15:09:24 -0500

    add vercel.json

--- server/app.js
+++ server/app.js
@@ -2,17 +2,20 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
+import {dirname, join} from 'path'
 import { fileURLToPath } from 'url';
 
 import pokemonsRoutes from "./routes/pokemons.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());  // CORS
+
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
@@ -22,7 +25,8 @@ app.use("/", indexRoutes);
 app.use("/api/pokemon", pokemonsRoutes);
 
 // Static files
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(join(__dirname, '../client/dist')))
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
--- server/controllers/index.rotes.js
+++ server/controllers/index.rotes.js
@@ -1,8 +1,21 @@
+// import { pool } from "../db.js";
+
+// export const index = (req, res) => res.json({ message: "Welcome to my api, by Astudillo Perez Edwin Uriel" });
+
+// export const ping = async (req, res) => {
+//   const [result] = await pool.query('SELECT "pong" as result');
+//   res.json(result[0]);
+// };
+
+import { Router } from "express";
 import { pool } from "../db.js";
 
-export const index = (req, res) => res.json({ message: "Welcome to my api, by Astudillo Perez Edwin Uriel" });
+const router = Router();
 
-export const ping = async (req, res) => {
-  const [result] = await pool.query('SELECT "pong" as result');
-  res.json(result[0]);
-};
+router.get("/ping", async (req, res) => {
+  const [rows] = await pool.query("SELECT 1 + 1 as result");
+  console.log(rows[0]);
+  res.json(rows[0]);
+});
+
+export default router;
